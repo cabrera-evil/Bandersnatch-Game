52bf3daaebcdfafda3621ecdd622fdde6e224063
cabrera-evil <douglascabrera.dev@gmail.com>
2022-04-12 11:57:11 -0600

    Update: C#

--- C#/Bandersnatch/Bandersnatch/Program.cs
+++ C#/Bandersnatch/Bandersnatch/Program.cs
@@ -4,6 +4,15 @@
     {
         public static void Main(string[] args)
         {
+            //weapon myWeapons[] = new weapon[4];            
+        }
+
+        public static void setWeapons()
+        {
+            /*private string name;
+            private double failure;
+            private int usage;
+            private double damage;*/
         }
     }
-}
+} 
