6c9eb6c813999973eabaa687e820fd4762745f7f
cabrera-evil <douglascabrera.dev@gmail.com>
2022-04-12 14:06:43 -0600

    Update: C#

---
+++ C#/Bandersnatch/Bandersnatch/shop.cs
@@ -0,0 +1,7 @@
+﻿namespace Bandersnatch
+{
+    public class shop
+    {
+        
+    }
+}
---
+++ C#/Bandersnatch/Bandersnatch/weapon.cs
@@ -0,0 +1,87 @@
+﻿namespace Bandersnatch
+{
+    public abstract class weapon
+    {
+        protected int id;
+        protected string name;
+        protected double failure;
+        protected int usage;
+        protected double damage;
+
+        protected weapon(int id, string name, double failure, int usage, double damage)
+        {
+            this.id = id;
+            this.name = name;
+            this.failure = failure;
+            this.usage = usage;
+            this.damage = damage;
+        }
+    }
+    public class Sword : weapon
+    {
+        public Sword(int id, string name, double failure, int usage, double damage) : base(id, name, failure, usage, damage)
+        {
+            this.id = id;
+            this.name = name;
+            this.failure = failure;
+            this.usage = usage;
+            this.damage = damage;
+        }
+    }
+
+    public class BowAndArrow : weapon
+    {
+        public BowAndArrow(int id, string name, double failure, int usage, double damage) : base(id, name, failure, usage, damage)
+        {
+            this.id = id;
+            this.name = name;
+            this.failure = failure;
+            this.usage = usage;
+            this.damage = damage;
+        }
+    }
+    public class MagicStaff : weapon
+    {
+        public MagicStaff(int id, string name, double failure, int usage, double damage) : base(id, name, failure, usage, damage)
+        {
+            this.id = id;
+            this.name = name;
+            this.failure = failure;
+            this.usage = usage;
+            this.damage = damage;
+        }
+    }
+    public class FireBall : weapon
+    {
+        public FireBall(int id, string name, double failure, int usage, double damage) : base(id, name, failure, usage, damage)
+        {
+            this.id = id;
+            this.name = name;
+            this.failure = failure;
+            this.usage = usage;
+            this.damage = damage;
+        }
+    }
+    public class Crossbow : weapon
+    {
+        public Crossbow(int id, string name, double failure, int usage, double damage) : base(id, name, failure, usage, damage)
+        {
+            this.id = id;
+            this.name = name;
+            this.failure = failure;
+            this.usage = usage;
+            this.damage = damage;
+        }
+    }
+    public class Axe : weapon
+    {
+        public Axe(int id, string name, double failure, int usage, double damage) : base(id, name, failure, usage, damage)
+        {
+            this.id = id;
+            this.name = name;
+            this.failure = failure;
+            this.usage = usage;
+            this.damage = damage;
+        }
+    }
+}
